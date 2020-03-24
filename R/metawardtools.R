6ffc4930a3bff86c3e2d67ab16cfa6f4c5410cf9
Rob Challen <rob@terminological.co.uk>
2020-03-24 16:18:13 +0000

    fix smart apostrophe

--- R/metawardtools.R
+++ R/metawardtools.R
@@ -1,9 +1,15 @@
-readSpatialFile<-function (fname){        # read and process a single file
+#' loads a spatial model file
+#' 
+#' @param fname - a filename containing a ward based metaward model .dat file
+#' @import dplyr
+#' @return a data frame containing the simulation results
+#' @export
+readSpatialFile = function(fname) {        # read and process a single file
   #  Function to read in Work/PlayInfections.dat’ type files (many columns, one for each class only of infecteds, Time is row number)
   tmp = read.table(file=fname,sep = ' ') %>% # make long data frame
     mutate(time=row_number()) %>% # count up time, as row number
     pivot_longer(-time) %>%
-    mutate(ward=as.integer(str_remove(name,‘V’))) %>%
+    mutate(ward=as.integer(str_remove(name,'V'))) %>%
     select(-name)
   
   tmp = tmp %>% left_join(UKWardLookup %>% select(ward = FID,WD11CD,LAD11CD), by="ward")
@@ -13,4 +19,3 @@ readSpatialFile<-function (fname){        # read and process a single file
 
 
 
-readSpatialFile("~/Dropbox/covid19/ventilator-demand/ForMattData.dat")
